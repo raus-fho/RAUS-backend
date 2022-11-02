8b7646922739f5c0b5a4da832cd66eac38e6a73f
Caio <caiocfer@gmail.com>
2022-11-02 18:29:05 -0300

    Added update route
    It's now possible to receive a JSON that will update the database.
    
    Signed-off-by: Caio <caiocfer@gmail.com>

--- app/api/db/dbapi.py
+++ app/api/db/dbapi.py
@@ -1,6 +1,6 @@
 from app.api.db import models
 from .db import SessionLocal, engine
-from flask import Flask, _app_ctx_stack, jsonify, url_for
+from flask import Flask, jsonify, request
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
@@ -10,6 +10,14 @@ app.session = scoped_session(session_factory=sessionmaker(bind=engine))
 def get_current_status():
     status = app.session.query(models.Status).first()
     return row2dict(status)
+
+def update_status():
+    status = jsonify(request.json).json 
+    status.update({'actuator_status':status['actuator_status']})
+    app.session.query(models.Status)\
+        .update(status)
+    app.session.commit()
+    return status
 
 def row2dict(row):
     d = {}
--- app/api/routes.py
+++ app/api/routes.py
@@ -6,3 +6,8 @@ from app.api.db import dbapi
 def get_status():
     status = dbapi.get_current_status()
     return jsonify(status)
+
+@app.route('/status' ,methods=["POST"])
+def update_status():
+    status = dbapi.update_status()
+    return status
